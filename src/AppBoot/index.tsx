c380e21806a00103d94f5d637047f3c29a1e8b1a
manvigautam09 <mnvsgautam9@gmail.com>
2021-06-06 00:03:47 +0530

    added default login template and redux setup

--- src/AppBoot/index.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import 'react-native-gesture-handler';
-
-import RootNavigator from '../Navigators';
-
-const AppBoot = () => {
-  return <RootNavigator />;
-};
-export default AppBoot;
--- src/Navigators/LoginScreen/index.tsx
+++ src/Navigators/LoginScreen/index.tsx
@@ -1,12 +1,56 @@
-import React from 'react';
-import {SafeAreaView, Text} from 'react-native';
+import React, {useMemo, useState} from 'react';
+import {SafeAreaView, TextInput, StyleSheet, Button} from 'react-native';
 
 const LoginScreen = () => {
+  const [userName, setUserName] = useState('');
+  const [password, setPassword] = useState('');
+
+  const loginButtonDisabled = useMemo(
+    () => userName.length === 0 || password.length === 0,
+    [password.length, userName.length],
+  );
+
   return (
-    <SafeAreaView>
-      <Text>This is Login Screen</Text>
+    <SafeAreaView style={styles.container}>
+      <TextInput
+        value={userName}
+        style={styles.input}
+        placeholder={'Username'}
+        onChangeText={username => setUserName(username)}
+      />
+      <TextInput
+        value={password}
+        style={styles.input}
+        secureTextEntry={true}
+        placeholder={'Password'}
+        onChangeText={passWord => setPassword(passWord)}
+      />
+
+      <Button
+        color="blue"
+        title={'Login'}
+        disabled={loginButtonDisabled}
+        onPress={() => console.log('###onPress', userName, password)}
+      />
     </SafeAreaView>
   );
 };
 
 export default LoginScreen;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#ecf0f1',
+  },
+  input: {
+    width: 200,
+    height: 44,
+    padding: 10,
+    borderWidth: 1,
+    borderColor: 'black',
+    marginBottom: 10,
+  },
+});
